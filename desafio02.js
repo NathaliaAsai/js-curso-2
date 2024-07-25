d487d3c114ae4b719bcabde6388a8da3f16085ad
Nathalia Harumi Asai <145270943+NathaliaAsai@users.noreply.github.com>
2024-07-25 17:47:32 -0300

    desafio03

--- desafio02.js
+++ desafio02.js
@@ -1,10 +1,29 @@
 //Criar uma função que exibe "Olá, mundo!" no console.
+function texto (){
+ console.log("Olá, mundo!");
+}
+texto ();
 
 //Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+function textoNome(nome) {
+ console.log('Ola, ${nome}!');
+}
+ textoNome('Name');
+
 
 //Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+function parametroNunero(numero) {
+ return número * 2;
+}
+
+ let resultDobro = parametroNumero
+console.log(resultDobro);
 
 //Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function prompt() {
+ let numeroUm('Digite o primeiro numero');
+
+
 
 //Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
 
