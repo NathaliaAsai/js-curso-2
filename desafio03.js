c0edc9adad756b535dc1450af92bbdcbd567e608
Nathalia Harumi Asai <145270943+NathaliaAsai@users.noreply.github.com>
2024-07-26 11:25:23 -0300

    Atualizar primeiro exercicio do desafio03.js

--- desafio03.js
+++ desafio03.js
@@ -1,13 +1,41 @@
 //Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
 
+
+function imc(peso, altura) {
+ let peso = prompt('Qual é o seu peso em kg?'(ex.: 60.5);
+function 
+ let altura = prompt('Qual é a sua altura em m? (ex.: 1.70);
+
+ let alturaQuadrado = altura * altura;
+ return peso/alturaQuadrado
+}
+
+console.log(seu imc é ${imc});
+if (imc>40.0){
+ console.log('Obesidde grave - grau III');
+ } else if (imc>=30.0 && <40.0){
+ console.log('obesidade - grau II');
+ } else if (imc >=25.0 && <30.0){
+ console.log('sobrepeso - grau I,);
+ } else if (imc>=18.5 && <25.0){
+ console.log('normal');
+ }else if (imc<18.5)
+}
+ 
+
 //Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+
 
 //Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
 
+
 //Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+
 
 //Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
 
+
 //Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+
 
 
