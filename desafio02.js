2bdcfb8d14b673ef98c4d2b956c7fc0f80d8be7f
Nathalia Harumi Asai <145270943+NathaliaAsai@users.noreply.github.com>
2024-07-25 21:08:36 -0300

    desafio 2 realizado

--- desafio02.js
+++ desafio02.js
@@ -1,17 +1,17 @@
-//Criar uma função que exibe "Olá, mundo!" no console.
+//01 - Criar uma função que exibe "Olá, mundo!" no console.
 function texto (){
  console.log("Olá, mundo!");
 }
 texto ();
 
-//Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+//02 - Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
 function textoNome(nome) {
  console.log('Ola, ${nome}!');
 }
  textoNome('Name');
 
 
-//Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+//03 - Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
 function parametroNunero(numero) {
  return número * 2;
 }
@@ -19,12 +19,48 @@ function parametroNunero(numero) {
  let resultDobro = parametroNumero
 console.log(resultDobro);
 
-//Criar uma função que recebe três números como parâmetros e retorna a média deles.
-function prompt() {
- let numeroUm('Digite o primeiro numero');
+//04 - Criar uma função que recebe três números como parâmetros e retorna a média deles.
+function solicitarNumero() {
+ let numeroUm = prompt(`Digite o primeiro número`);
+ let numeroDois = prompt(`Digite o segundo número`);
+ let numeroTres = prompt(`Digite o terceiro número`);
+
+ numeroUm = parseFloat(numeroUm);
+ numeroDois = parseFloat(numeroDois);
+ numeroTres = parseFloar(numeroTres);
+
+let somatoria = numeroUm + numeroDois + numeroTres;
+let media = somatoria/3;
+return media;
+}
+
+let resultado = solicitarNumero() //Chama a função e guarda o resultado
+console.log(resultado);
+
+//05 - Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+function solicitarNumero() {
+    let numeroA = prompt('Digite um número');
+    let numeroB = prompt('Digite outro número');
+
+    if( parseFloat(numeroA) > parseFloat(numeroB)) {
+        return numeroA;
+    } else {
+        return numeroB;
+    }
+}
+let resultado = solicitarNumero();
+console.log(resultado);
 
 
+//06 - Criar uma função que recebe um número como parâmetro e retorna o resultado
+//da multiplicação desse número por ele mesmo
+function numeroSolicitado() {
+    let numero = prompt('Digite um número');
+    numero = parseFloat(numero)
 
-//Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+    let multiplicacao = numero * numero;
+    return multiplicacao;
+}
 
-//Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+let aoQuadrado = numeroSolicitado();
+console.log(aoQuadrado);
