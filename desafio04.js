7b6f1750955ff97b2de8b6c11cb4df239c6b0db8
Nathalia Harumi Asai <145270943+NathaliaAsai@users.noreply.github.com>
2024-07-28 23:46:12 -0300

    desaio 04 realizado

--- desafio04.js
+++ desafio04.js
@@ -1,11 +1,21 @@
 //Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = []
 
 //Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let lingProg = ['JavaScript','C','C++', 'Kotlin','Python'];
 
 //Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+lingProg.push('Java','Ruby', 'GoLang');
 
 //Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let nomes = ['Fifi', 'Kinder', 'Kitty']
+console.log(nomes[0]);
 
 //Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let nomes = ['Fifi', 'Kinder', 'Kitty']
+console.log(nomes[1]);
 
 //Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+let nomes = ['Fifi', 'Kinder', 'Kitty']
+nomes.length
+console.log(nomes[2]);
