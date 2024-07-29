a144e737cdc0c293367977ee9fdcab548f5d11fa
Nathalia Harumi Asai <145270943+NathaliaAsai@users.noreply.github.com>
2024-07-28 23:02:37 -0300

    desafio 03 finalizado

--- desafio03.js
+++ desafio03.js
@@ -25,7 +25,20 @@ if (imcCalc>40.0){
  
 
 //Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+let numero = parseInt(prompt(`Digite um número inteiro`));
+let fatorial = 1;
 
+function calcNumero(numero) {
+    while(numero > 0) {
+    fatorial *= numero; //multiplica fatorial por numero
+    numero;
+    }
+    return fatorial;
+}
+
+let numeroFatoiral = calcNumero(numero); 
+console.log(`${numero}! = ${numeroFatorial}.`);
+    
 
 //Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
 let valor = parseFloat(prompt(`Digite o valor em dólar: U$`));
@@ -36,16 +49,57 @@ function conversao(valor) {
 }
 
 let valorEmReais = conversao(valor);
-console.log(O valor em reais é: R$ ${valorEmReais.toFixed(2)}`);
+console.log(`O valor em reais é: R$ ${valorEmReais.toFixed(2)}`);
 
 
 //Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
 
+let altura = parseFloat(prompt(`Qual é a altura?`));
+let largura = parseFloat(prompt(`Qual é a largura?`));
+
+function calcArea(altura, largura) { 
+    let area = altura * largura;
+    return area;
+}
+
+function calcPerimetro(altura, largura) {
+    let perimetro = 2 * (altura + largura);
+    return perimetro;
+}
+
+let areaSala = calcArea(altura, largura);
+let perimetroSala = calcPerimetro(altura, largura); 
+console.log(`A sala com ${altura}m de altura e ${largura}m de largura possui ${areaSala}m de área, e seu perímetro é de ${perimetroSala}`);
 
 //Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+let area = parseFloat(prompt(`Qual o raio da sala?`));
 
+function calcArea(raio) { 
+    let area = 3.14 * raio * raio
+    return area;
+}
+
+function calcPerimetro(raio) {
+    let perimetro = 2 * 3.14 * raio;
+    return perimetro;
+}
+
+let areaSala = calcArea(raio);
+let perimetroSala = calcPerimetro(raio); 
+alert(`A sala circular com ${raio}m de raio possui ${areaSala.toFixed(2)}m.^2 de área, e seu perímetro é de ${perimetroSala.toFixed(2)}m.`);
+    
 
 //Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+let numero = parseInt(prompt(`Digite um número`));
+let contador = 0
 
+function calcNumero (numero, contador) {
+    let tabuada = numero * contador
+    return tabuada;
+}
 
-
+while(contador <=10) {
+    let tabuadaNumero = calcNumero(numero, contador);
+    console.log(`${numero} x ${contador} = ${tabuadaNumero}.`)
+    contador++
+}
